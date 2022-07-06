78ae46fe6d93d2a7e8521fe06409ff91e9e3d8d2
Stephanie smith <smithse4@gmail.com>
2022-07-05 23:28:55 -0400

    back end api routes running

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,59 +1,59 @@
-// const boneForm = document.querySelector('#bone-form');
-// const antag = document.querySelector('#antagonist');
-// const bone = document.querySelector('#bone');
-// const boneSubmit = document.querySelector('#bone-submit');
-// const boneList = document.querySelector('#all-bones-list');
+const boneForm = document.querySelector('#bone-form');
+const antag = document.querySelector('#antagonist');
+const bone = document.querySelector('#bone');
+const boneSubmit = document.querySelector('#bone-submit');
+const boneList = document.querySelector('#all-bones-list');
 
 
-// const collectBone = async function(event) {
-//     event.preventDefault();
+const collectBone = async function(event) {
+    event.preventDefault();
     
-//     let newBone = {
-//         antagonist: antag.value,
-//         text: bone.value
-//     }
+    let newBone = {
+        antagonist: antag.value,
+        bone: bone.value
+    }
 
-//     await fetch(`/api/bones`, {
-//         method: 'POST',
-//         body: JSON.stringify(newBone),
-//         headers: { 'Content-Type': 'application/json' },
-//     });
+    await fetch(`/api/bones`, {
+        method: 'POST',
+        body: JSON.stringify(newBone),
+        headers: { 'Content-Type': 'application/json' },
+    });
 
-//     displayBones();
+    displayBones();
 
-//     antag.value = "";
-//     bone.value = "";
-// }
+    antag.value = "";
+    bone.value = "";
+}
 
-// const getBones = () => 
-//     fetch('/api/bones', {
-//         method: 'GET',
-//         headers: {
-//         'Content-Type': 'application/json',
-//         },
-//     })
+const getBones = () => 
+    fetch('/api/bones', {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    })
 
-// async function displayBones(data) {
-//     let bones = await data.json();
-//     if(bones) {
-//         boneList.innerHTML = "";
-//         for(let i = 0; i < bones.length; i++) {
-//             let card = document.createElement('div');
-//             let cardHead = document.createElement('h3');
+async function displayBones(data) {
+    let bones = await data.json();
+    if(bones) {
+        boneList.innerHTML = "";
+        for(let i = 0; i < bones.length; i++) {
+            let card = document.createElement('div');
+            let cardHead = document.createElement('h3');
 
-//             card.setAttribute('class', 'bone-card');
+            card.setAttribute('class', 'bone-card');
 
-//             cardHead.innerHTML = `I have a bone to pick with <span class="bone-card-antag">${bones[i].antagonist}</span> ${bones[i].text}`;
+            cardHead.innerHTML = `I have a bone to pick with <span class="bone-card-antag">${bones[i].antagonist}</span> ${bones[i].bone}`;
 
-//             card.append(cardHead);
-//             boneList.append(card);
-//         }
+            card.append(cardHead);
+            boneList.append(card);
+        }
 
-//     }
-// }
+    }
+}
 
-// const getAndRenderBones = () => getBones().then(displayBones);
+const getAndRenderBones = () => getBones().then(displayBones);
 
-// getAndRenderBones();
+getAndRenderBones();
 
-// boneForm.addEventListener('submit', collectBone);
+boneForm.addEventListener('submit', collectBone);
