610b07fa1c04e2d970be073a1d67e18ab911893a
Stephanie smith <smithse4@gmail.com>
2022-07-05 23:37:33 -0400

    added html routes, need to fix refresh to show new posts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const views = require('./view');
 const apiRoutes = require('./api');
 
+router.use('/', views);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 app.use(routes);
 
 db.once('open', () => {
