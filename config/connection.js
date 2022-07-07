5138f6680ff930f45afa85dac16526090a203fa5
Stephanie smith <smithse4@gmail.com>
2022-07-07 17:40:00 -0400

    connection update

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bonetopick', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
